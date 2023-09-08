f2701d6815a0c42f99cbedbdf74d8882b87e4585
Valentin Perignon <valentin.perignon@icloud.com>
2023-09-08 19:33:47 +0200

    Initial Commit

---
+++ CloudyVision/CloudyVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudyVisionApp.swift
+//  CloudyVision
+//
+//  Created by Valentin Perignon on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CloudyVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ForecastView()
+        }
+    }
+}
---
+++ CloudyVision/Views/ForecastView.swift
@@ -0,0 +1,22 @@
+//
+//  ForecastView.swift
+//  CloudyVision
+//
+//  Created by Valentin Perignon on 08/09/2023.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ForecastView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ForecastView()
+}
