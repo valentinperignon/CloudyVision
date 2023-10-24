15b0686fbac5e552f6bb66bb9e03d672fcd8b5f9
Valentin Perignon <valentin.perignon@icloud.com>
2023-10-24 22:31:48 +0200

    chore: Prepare daily forecast view

---
+++ CloudyVision/Views/PlacesForecast/Forecast/Items/DailyForecastView.swift
@@ -0,0 +1,17 @@
+//
+//  DailyForecastView.swift
+//  CloudyVision
+//
+//  Created by Valentin Perignon on 24/10/2023.
+//
+
+import SwiftUI
+import WeatherKit
+
+struct DailyForecastView: View {
+    let dailyForecast: Forecast<DayWeather>
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
